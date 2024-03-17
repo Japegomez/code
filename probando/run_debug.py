bc51a6b5c34df88690d8b04e4df800dc446a8dc7
Javier Peña Gomez <japegomez@opendeusto.es>
2024-03-17 20:13:14 +0100

    add advanced laboratory

---
+++ probando/run_debug.py
@@ -0,0 +1,13 @@
+from mylab import create_app, socketio
+import os
+from gevent import monkey
+monkey.patch_all()
+
+
+if os.environ.get('FLASK_DEBUG') == '1':
+    config_name = 'development'
+else:
+    config_name = os.environ.get('FLASK_CONFIG') or 'default'
+
+app = create_app(config_name)
+socketio.run(app)
