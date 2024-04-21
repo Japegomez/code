90f87f8f247d943d9b9d8505fc554caed10d6404
Javier Peña Gomez <japegomez@opendeusto.es>
2024-04-21 20:45:08 +0200

    add config parameters

--- remoteLab/src/app/en-paralelo/en-paralelo.component.ts
+++ remoteLab/src/app/en-paralelo/en-paralelo.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-en-paralelo',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './en-paralelo.component.html',
   styleUrl: './en-paralelo.component.css'
 })
 export class EnParaleloComponent {
-
+  numRamasEnParalelo = '2ramas';
 }
