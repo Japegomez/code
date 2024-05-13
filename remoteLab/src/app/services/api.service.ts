8c088a6a37a3313d33297851f81351bdcfa27aed
Javier Peña Gomez <japegomez@opendeusto.es>
2024-05-13 17:59:20 +0200

    use API for first time

--- remoteLab/src/app/services/api.service.ts
+++ remoteLab/src/app/services/api.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserSession } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
+  user!: UserSession;
+  constructor(private http: HttpClient ) { 
+  }
+  getSessionConfig(): Observable<UserSession> {
+    return this.http.get<UserSession>('http://localhost:5000/api/v1/config');
+  }
 
-  constructor() { }
+  sendLabConfig(typeCircuit: string, subtype: string) {
+    return this.http.post('localhost:5000/api/v1/lab/', {typeCircuit, subtype});
+  }
+
+
 }
+
