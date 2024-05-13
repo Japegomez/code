f0cfa0c53366ec9a0ad705f75cfc84a0b3746da9
Javier Peña Gomez <japegomez@opendeusto.es>
2024-05-13 18:01:05 +0200

    add timer

--- remoteLab/src/app/components/timer/timer.component.ts
+++ remoteLab/src/app/components/timer/timer.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CountdownComponent, CountdownConfig, CountdownEvent } from 'ngx-countdown';
+
+const KEY = 'time';
+const DEFAULT = 4;
 
 @Component({
   selector: 'app-timer',
+  host: {
+    '[class.card]': `true`,
+    '[class.text-center]': `true`,
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [],
   templateUrl: './timer.component.html',
-  styleUrl: './timer.component.css'
+  imports: [CountdownComponent],
 })
-export class TimerComponent {
-  username = 'javi';
-  isLogged = true;
+export class TimerComponent implements OnInit {
+  config: CountdownConfig = { leftTime: DEFAULT, notify: 0 };
+  ngOnInit(): void {
+    let value = +localStorage.getItem(KEY)!!;
+    if (value <= 0) value = DEFAULT;
+    this.config = { ...this.config, leftTime: value };
+  }
+
+  handleEvent(ev: CountdownEvent) {
+    if (ev.action === 'notify') {
+      // Save current value
+      localStorage.setItem(KEY, `${ev.left / 1000}`);
+
+      if ( ev.left === 0 ) {
+        // Reset value
+        console.log('Time is up!');
+      }
+    }
+  }
 }
